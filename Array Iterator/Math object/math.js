b222fa83a52792c60cc1be19c2c3c33761d6ed08
amul patle <amulpatle3@gmail.com>
2023-02-04 20:36:15 +0530

    Java Script Beginning

---
+++ Array Iterator/Math object/math.js
@@ -0,0 +1,22 @@
+// Math
+// standard built-in objects - always available
+
+// const number = 4.5677;
+// const result = Math.floor(number);
+// console.log(result);
+
+// const ans = Math.ceil(8.999999);
+// console.log(ans);
+
+// const number = 25;
+// const result = Math.sqrt(number);
+
+// console.log(result);
+
+//const result = Math.min(4, 5, 6, 8, 9, 1);
+
+// const result = Math.max(4, 5, 6, 8, 9, 1);
+// console.log(result);
+
+const result = Math.floor(Math.random() * 10 + 1);
+console.log(result);
