72f58ab2b459bac268ad57b7ec4c5da4cc7fd7b9
amul patle <amulpatle3@gmail.com>
2023-02-05 10:17:13 +0530

    DOM manipulation

---
+++ DOM/script.js
@@ -0,0 +1,10 @@
+// const heading = document.getElementsByTagName("h1");
+// console.log(heading);
+
+// querry selector
+// const heading = document.querySelector(".heading2");
+// console.log(heading);
+
+// querry selector all
+const heading = document.querySelectorAll(".heading");
+console.log(heading);
