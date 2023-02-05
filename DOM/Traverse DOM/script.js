48963bcd786ad44bd246037ab15aafe6564949ae
amul patle <amulpatle3@gmail.com>
2023-02-05 11:56:06 +0530

    DOM Manipulation

--- DOM/Traverse DOM/script.js
+++ DOM/Traverse DOM/script.js
@@ -1,1 +1,23 @@
 //Traverse DOM
+
+// 1 parentNode
+
+// const heading = document.querySelector(".heading");
+
+// const parent = heading.parentNode;
+// console.log(parent);
+
+// 2. childNodes
+// const parent = document.querySelector(".parent");
+
+// console.log(parent.childNodes);
+
+// 3 nextElementsibing
+
+// const heading = document.querySelector(".heading");
+// console.log(heading.nextElementSibling);
+
+//  4.previousElementsibing
+
+const subHeading = document.querySelector("h3");
+console.log(subHeading.previousElementSibling);
