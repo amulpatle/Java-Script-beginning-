80488a18ae58e935c7b53375a9df1f29d9cff39b
amul patle <amulpatle3@gmail.com>
2023-02-10 17:00:40 +0530

    getAttribute() & SetAttribute()

---
+++ DOM John Smilga/GetAttribute() , SetAttribute()/script.js
@@ -0,0 +1,17 @@
+// getAttribute();
+// setAttribute()
+
+const first = document.querySelector(".first");
+const idValue = first.getAttribute("id");
+console.log(idValue);
+
+const link = document.getElementById("link");
+const showLink = link.getAttribute("href");
+console.log(showLink);
+
+const last = link.nextElementSibling;
+last.setAttribute("class", "first");
+last.textContent = "i also have text content";
+console.log(last);
+
+const links = document.querySelectorAll(".first");
