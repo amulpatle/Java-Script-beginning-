f7c09b6f781ab99df61746d3aaab41a02b4a5be8
amul patle <amulpatle3@gmail.com>
2023-02-11 00:30:59 +0530

    css style

---
+++ DOM John Smilga/change css with style property/script.js
@@ -0,0 +1,10 @@
+//css
+const random = document.querySelector(".random");
+console.log(random.style);
+
+// random.style.backgroundColor = "blue";
+// random.style.Color = "white";
+// random.style.fontSize = "3rem";
+// random.style.textTransform = "capitalize";
+
+random.classList.add("title");
