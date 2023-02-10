f8924d4a3a9066e1a7f040295609761b70ca65fc
amul patle <amulpatle3@gmail.com>
2023-02-10 15:42:06 +0530

    nextElementSibling & PreviousElementSibling

---
+++ DOM John Smilga/Navigate The Dom/previousElementSibling & nextELementSibling/script.js
@@ -0,0 +1,5 @@
+// previousElementSibling
+// nextElementSibling
+const first = document.querySelector(".first");
+first.nextElementSibling.style.color = "red";
+const last = document.querySelector("#last");
