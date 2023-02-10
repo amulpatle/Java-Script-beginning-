c4bbae242afee85c419b924636a96591aaa9c742
amul patle <amulpatle3@gmail.com>
2023-02-10 18:18:03 +0530

    classList & className

---
+++ DOM John Smilga/classList & className/script.js
@@ -0,0 +1,16 @@
+// className
+// classList
+
+const first = document.getElementById("first");
+const second = document.getElementById("second");
+const third = document.getElementById("third");
+
+// const classValue = first.className;
+console.log(classValue);
+
+// second.className = "colors";
+// second.className = "colors text";
+
+third.classList.add("colors");
+const classValue = third.className;
+console.log(classValue);
