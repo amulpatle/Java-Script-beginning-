0756060b42d7db72ee1ed84ff611c2d2d79a7f8a
amul patle <amulpatle3@gmail.com>
2023-02-10 23:59:32 +0530

    remove & removeChild

---
+++ DOM John Smilga/Remove & RemoveChild/script.js
@@ -0,0 +1,8 @@
+// remove
+//removeChild
+
+const result = document.querySelector("#result");
+// result.remove();
+const heading = result.querySelector("h1");
+result.removeChild(heading);
+console.log(heading);
