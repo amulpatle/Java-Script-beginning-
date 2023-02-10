f529be0046e078af06f8278757ddeda35fded240
amul patle <amulpatle3@gmail.com>
2023-02-10 12:47:30 +0530

    Dom Manipulation

---
+++ DOM John Smilga/Get Element by Class name/script.js
@@ -0,0 +1,10 @@
+// Select the element or group of element of element the we want
+// Decide the effect we want to apply to the selection
+
+// getElementByClassName('classname');
+
+// index, lenght property but not array methods
+
+const listItems = document.getElementsByClassName("special");
+console.log(listItems);
+listItems[1].style.color = "blue";
