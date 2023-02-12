78c81c41a30aa8a9d5581e416665073619595d19
amul patle <amulpatle3@gmail.com>
2023-02-12 17:35:59 +0530

    Refector

--- DOM Project/3. Manu project/app.js
+++ DOM Project/3. Manu project/app.js
@@ -74,7 +74,11 @@ const menu = [
 ];
 const sectionCenter = document.querySelector(".section-center");
 window.addEventListener("DOMContentLoaded", function () {
-  let displayMenu = menu.map(function (item) {
+  displayMenuItem(menu);
+});
+
+function displayMenuItem(menuItem) {
+  let displayMenu = menuItem.map(function (item) {
     return `<article class="menu-item">
           <img src="${item.img}" alt="${item.title}" class="photo" />
           <div class="item-info">
@@ -90,4 +94,4 @@ window.addEventListener("DOMContentLoaded", function () {
   });
   displayMenu = displayMenu.join("");
   sectionCenter.innerHTML = displayMenu;
-});
+}
