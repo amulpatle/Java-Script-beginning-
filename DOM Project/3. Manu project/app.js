5a2207a07cac811d06dca006aef71056015e0c56
amul patle <amulpatle3@gmail.com>
2023-02-13 23:10:55 +0530

    filter project & menu project

--- DOM Project/3. Manu project/app.js
+++ DOM Project/3. Manu project/app.js
@@ -73,8 +73,28 @@ const menu = [
   },
 ];
 const sectionCenter = document.querySelector(".section-center");
+
+const filterBtns = document.querySelectorAll(".filter-btn");
+//load items
 window.addEventListener("DOMContentLoaded", function () {
   displayMenuItem(menu);
+});
+//filet items
+
+filterBtns.forEach(function (btn) {
+  btn.addEventListener("click", function (e) {
+    const category = e.currentTarget.dataset.id;
+    const menuCategory = menu.filter(function (menuItem) {
+      if (menuItem.category === category) {
+        return menuItem;
+      }
+    });
+    if (category === "all") {
+      displayMenuItem(menu);
+    } else {
+      displayMenuItem(menuCategory);
+    }
+  });
 });
 
 function displayMenuItem(menuItem) {
